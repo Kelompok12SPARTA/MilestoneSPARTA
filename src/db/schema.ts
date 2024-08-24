9897514230ad5ac9d08ac66d990e65833d5efefe
susTuna <hidasetsuna@gmail.com>
2024-08-24 13:45:44 +0700

    turso db and drizzle connected

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,22 +1,16 @@
 import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const restaurantTable = sqliteTable('restaurant', {
+export const restaurantTable = sqliteTable('Restaurant', {
   id: integer('id').primaryKey(),
   name: text('name').notNull(),
-  rating: integer('rating').notNull(),  
-});
-export const productsTable = sqliteTable('product', {
-  id: integer('id').primaryKey(),
-  name: text('name').notNull(),
-  category: text('category').notNull(),
-  restaurant: text('restaurant').unique().notNull(),
-  price: integer('price').notNull(),
+  address: text('address').notNull(),
   rating: integer('rating').notNull(),
+  price: integer('price').notNull(),
+  distance: integer('distance').notNull(),
+  photo: text('photo').notNull(),
+  link: text('link').notNull(),
 });
 
 export type InsertRestaurant = typeof restaurantTable.$inferInsert;
 export type SelectRestaurant = typeof restaurantTable.$inferSelect;
-
-export type InsertProducts = typeof productsTable.$inferInsert;
-export type SelectProducts = typeof productsTable.$inferSelect;
