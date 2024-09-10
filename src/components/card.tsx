e1ea17e439019d0f78b733fcf6c584386b0b72de
Setsuna <148179846+susTuna@users.noreply.github.com>
2024-09-10 16:11:09 +0700

    changed text color to black by default

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -18,8 +18,8 @@ function Card({ id, title, description, image, altText, path }: CardProps) {
         className={`hover:scale-[1.01] transition-all shadow-md flex justify-between rounded-xl w-full p-4 bg-gradient-to-r  ${id === 1 ? "from-[#FFD8D8]" : id === 2 ? "from-[#F6E3BE]" : id === 3 ? "from-[#F0EAAC]" : "from-[#CCE0AC]"} to-white to-90%`}
       >
         <div className="flex flex-col justify-center">
-          <h1 className="font-bold text-2xl">{title}</h1>
-          <p>{description}</p>
+          <h1 className="font-bold text-2xl text-black">{title}</h1>
+          <p className="text-black">{description}</p>
         </div>
         <div className="flex items-center object-cover w-[50px] h-[50px]">
           <Image src={image} alt={altText} className="w-[60px]" />
